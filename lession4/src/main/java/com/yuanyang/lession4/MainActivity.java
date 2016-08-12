e0b9b2b02f67edd7d79b781b79b59b916ee25669
yuanyang <yuan823978@qq.com>
2016-08-12 12:53:18 +0800

    让你得App使用Android 版本变化

---
+++ lession4/src/main/java/com/yuanyang/lession4/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yuanyang.lession4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 支持不同的版本
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
