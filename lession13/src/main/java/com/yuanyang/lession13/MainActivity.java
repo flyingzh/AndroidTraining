60caba97b21770fcb6c8f87e926401cd75dc01e1
yuanyang <yuan823978@qq.com>
2016-08-16 17:25:35 +0800

    startActivityForResult的使用

---
+++ lession13/src/main/java/com/yuanyang/lession13/MainActivity.java
@@ -0,0 +1,48 @@
+package com.yuanyang.lession13;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * startActivityForResult
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUESTCODE_CONTACT = 1;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pickContact();
+    }
+
+    private void pickContact() {
+        Intent intent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        startActivityForResult(intent, REQUESTCODE_CONTACT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUESTCODE_CONTACT && data != null && resultCode == Activity.RESULT_OK) {
+            Uri contactUri = data.getData();
+            String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+            Cursor cursor = getContentResolver().query(contactUri, projection, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            String number = cursor.getString(columnIndex);
+            Log.i(TAG, number);
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+}
