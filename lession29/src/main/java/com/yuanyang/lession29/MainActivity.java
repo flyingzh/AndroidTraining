404128f0a0b78d586db984b0993cd16427383ba4
yuanyang <yuan823978@qq.com>
2016-10-16 21:03:51 +0800

    在ToolBar中添加按钮

--- lession29/src/main/java/com/yuanyang/lession29/MainActivity.java
+++ lession29/src/main/java/com/yuanyang/lession29/MainActivity.java
@@ -3,6 +3,9 @@ package com.yuanyang.lession29;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * ToolBar
@@ -16,6 +19,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setHideOffset(20);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.meu_toolbar, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_favorite:
+                Toast.makeText(MainActivity.this, "点击了喜欢按钮", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_setting:
+                Toast.makeText(MainActivity.this, "点击了设置按钮", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
