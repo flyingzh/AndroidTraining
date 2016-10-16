5ed026fd5105393289bec73ba7dd7ffde6c8ee14
yuanyang <yuan823978@qq.com>
2016-10-16 12:57:06 +0800

    ToolBar

--- lession29/src/main/java/com/yuanyang/lession29/MainActivity.java
+++ lession29/src/main/java/com/yuanyang/lession29/MainActivity.java
@@ -1,19 +1,21 @@
-package com.yuanyang.lession28;
+package com.yuanyang.lession29;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 /**
- * Setting Up The AppBar
+ * ToolBar
  */
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHideOffset(20);
     }
-
-
 }
